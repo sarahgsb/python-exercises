8ffa4efdbf56c3aeb551bfd59080e086e9afae58
Sarah <sarahgsb@hotmail.com>
2021-10-05 10:38:10 -0300

    chapter 2

---
+++ Chapter 2 - Flow Control/fiveTimes.py
@@ -0,0 +1,3 @@
+print("My name is")
+for index in range(5):
+    print("Jimmy Five Times(" + str(index) + ")")
---
+++ Chapter 2 - Flow Control/vampire.py
@@ -0,0 +1,10 @@
+name = "Sarah"
+age = 3000
+if name == "Kira":
+    print("Hi, Kira")
+elif age < 12:
+    print("You are not Kira, kiddo.")
+elif age > 2000:
+    print("Unlike you, Kira is not an undead, immortal vampire.")
+elif age > 100:
+    print("You are not Kira, grannie.")
